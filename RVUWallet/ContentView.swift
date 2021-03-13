d6dfeab88b50513c5dcc183a315cac5b6b21967b
EdwardArr <earriba1@gmail.com>
2021-03-13 15:01:29 -0500

    Initial Commit

---
+++ RVUWallet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RVUWallet
+//
+//  Created by Edward Arribasplata on 3/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RVUWallet/RVUWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  RVUWalletApp.swift
+//  RVUWallet
+//
+//  Created by Edward Arribasplata on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct RVUWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
